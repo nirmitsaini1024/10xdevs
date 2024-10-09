7892aadb5e6a0c3a1d05ccf9ea22348f669214cb
Nirmit Saini <nirmitsaini24@gmail.com>
2024-10-09 15:56:35 +0530

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
-
-
-
 export default function Home() {
   return (
-    <div className="w-full h-screen bg-zinc-900 flex flex-col justify-center items-center gap-24">
-      <h1 className="text-white font-extrabold text-9xl">10xdevs</h1>
-      </div>
+    <div className="w-full h-screen bg-zinc-900 flex flex-col justify-center items-center gap-12 md:gap-24">
+      <h1 className="text-white font-extrabold text-5xl md:text-7xl lg:text-9xl">10xdevs</h1>
+    </div>
   );
 }
